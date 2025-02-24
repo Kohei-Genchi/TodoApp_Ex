d9dfc58a901c82603ed10f5afc479b0d466ab9a7
Kohei <will.soul1992@gmail.com>
2025-02-24 14:47:31 +0530

    Implement laravel/breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,30 +1,7 @@
-import '../css/app.css'; // Import Tailwind CSS
-import { createApp } from 'vue';
-import './bootstrap.js';
-import App from './layouts/app.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import './bootstrap';
 
+import Alpine from 'alpinejs';
 
-import axios from 'axios';
-import TodoList from '@/components/TodoList.vue';
+window.Alpine = Alpine;
 
-
-// const routes = [
-//   {
-//     path: '/',
-//     component: TodoList
-//   }
-// ];
-
-
-// const router = createRouter({
-//     history: createWebHistory(),
-//     routes,
-//   });
-
-  const app = createApp(App)
-    // .use(router);
-
-  app.config.globalProperties.$axios = axios;
-
-  app.mount('#app');
+Alpine.start();
